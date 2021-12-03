8d232e08d6ff47e92ae4fe42b442d9425f228341
tijs <tijs.de.graaff@hva.nl>
2021-12-03 16:37:54 +0100

    Working day 1

--- on-the-1st-day-of-christmas/index.js
+++ on-the-1st-day-of-christmas/index.js
@@ -2,13 +2,43 @@ let readToArray = require("../santasHelpers");
 
 (async () => {
   const sensorData = await readToArray("sensorData.txt", parseInt);
-  let depthIncreases = 0;
+  console.log(sonarSweep(sensorData), sonarSweepThreeMeasurements(sensorData));
+})();
+
+const sonarSweep = (sensorData) => {
   let lastMeasurement;
+  let depthIncreases = 0;
 
   sensorData.map((measurement) => {
     measurement > lastMeasurement ? depthIncreases++ : null;
     lastMeasurement = measurement;
   });
 
-  console.log(depthIncreases);
-})();
+  return depthIncreases;
+};
+
+const sonarSweepThreeMeasurements = (sensorData) => {
+  const windowMeasurementCount = 3;
+  let lastMeasurement = [];
+  let currentMeasurement = [];
+  let depthIncreases = 0;
+
+  sensorData.map((_, dataIndex) => {
+    currentMeasurement = [];
+    [...new Array(windowMeasurementCount)].map((_, windowIndex) => {
+      currentMeasurement.push(sensorData[dataIndex + windowIndex]);
+    });
+
+    if (lastMeasurement.length > 0) {
+      currentMeasurement.reduce(add) > lastMeasurement.reduce(add)
+        ? depthIncreases++
+        : null;
+    }
+
+    lastMeasurement = currentMeasurement;
+  });
+
+  return depthIncreases;
+};
+
+const add = (prev, curr) => prev + curr;
