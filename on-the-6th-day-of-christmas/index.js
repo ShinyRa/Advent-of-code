b36ea743fe582d82f13de215a3ce12feae547731
tijs <tijs.de.graaff@hva.nl>
2021-12-08 22:38:03 +0100

    Finished day 6

--- on-the-6th-day-of-christmas/index.js
+++ on-the-6th-day-of-christmas/index.js
@@ -1,17 +1,31 @@
-let { splitWithDelimiterToArray } = require("../santasHelpers");
-
-class AnglerFish {
-  age;
-
-  constructor(age) {
-    this.age = age;
-  }
-}
+let { splitWithDelimiterToArray, addPredicate } = require("../santasHelpers");
 
 (async () => {
-  const population = await splitWithDelimiterToArray(
+  const scan = await splitWithDelimiterToArray(
     "anglerFishPopulation.txt",
     parseInt
   );
-  console.log(population);
+
+  const SIMULATION_DAYS = 256;
+  const MAX_REPRODUCTION_TICK = 8;
+  const NEW_REPRODUCTION_TICK = 6;
+  let anglerFishAges = [...new Array(MAX_REPRODUCTION_TICK + 1)].fill(0);
+
+  scan.forEach((reproduction) => anglerFishAges[reproduction]++);
+
+  [...new Array(SIMULATION_DAYS)].forEach(() => {
+    const anglerFishOnDay = [...new Array(MAX_REPRODUCTION_TICK + 1)].fill(0);
+    anglerFishOnDay.forEach((_, index) => {
+      const amount = anglerFishAges[index];
+      if (!index) {
+        anglerFishOnDay[NEW_REPRODUCTION_TICK] += amount;
+        anglerFishOnDay[MAX_REPRODUCTION_TICK] += amount;
+      } else {
+        anglerFishOnDay[index - 1] += amount;
+      }
+    });
+    anglerFishAges = anglerFishOnDay;
+  });
+
+  console.log(anglerFishAges.reduce(addPredicate));
 })();
