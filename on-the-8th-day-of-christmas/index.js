2272bccc9a967ff7df709772e0ff940fcaaaedc5
tijs <tijs.de.graaff@hva.nl>
2021-12-09 23:48:42 +0100

    Working day 8, I'm no longer alive

--- on-the-8th-day-of-christmas/index.js
+++ on-the-8th-day-of-christmas/index.js
@@ -1,20 +1,173 @@
-let { splitWithDelimiterToArray, addPredicate } = require("../santasHelpers");
+let { readToArray, addPredicate } = require("../santasHelpers");
 
 (async () => {
-  const crabPositions = await splitWithDelimiterToArray(
-    "crabPositions.txt",
-    parseInt
+  const randomSegments = await readToArray("segments.txt");
+  const signalPatterns = [];
+
+  [...randomSegments].forEach((signalPattern) => {
+    signalPatterns.push(signalPattern);
+  });
+
+  let digits = [];
+  [...signalPatterns].forEach((sequence) => {
+    digits.push(decryptSequence(sequence));
+  });
+  console.log(digits.map((digit) => parseInt(digit)).reduce(addPredicate));
+})();
+
+const decryptSequence = (sequence) => {
+  const digitLengths = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
+  const knownDigits = [1, 4, 7, 8];
+  const segments = ["a", "b", "c", "d", "e", "f", "g"];
+  const sides = [
+    "LEFT_TOP",
+    "RIGHT_TOP",
+    "TOP",
+    "CENTER",
+    "LEFT_BOTTOM",
+    "RIGHT_BOTTOM",
+    "BOTTOM",
+  ];
+
+  const digitSides = [
+    ["TOP", "RIGHT_TOP", "LEFT_TOP", "LEFT_BOTTOM", "RIGHT_BOTTOM", "BOTTOM"],
+    ["RIGHT_TOP", "RIGHT_BOTTOM"],
+    ["TOP", "RIGHT_TOP", "CENTER", "LEFT_BOTTOM", "BOTTOM"],
+    ["TOP", "RIGHT_TOP", "CENTER", "RIGHT_BOTTOM", "BOTTOM"],
+    ["RIGHT_TOP", "LEFT_TOP", "CENTER", "RIGHT_BOTTOM"],
+    ["TOP", "LEFT_TOP", "CENTER", "RIGHT_BOTTOM", "BOTTOM"],
+    ["TOP", "LEFT_TOP", "CENTER", "RIGHT_BOTTOM", "LEFT_BOTTOM", "BOTTOM"],
+    ["TOP", "RIGHT_TOP", "RIGHT_BOTTOM"],
+    [
+      "TOP",
+      "RIGHT_TOP",
+      "LEFT_TOP",
+      "CENTER",
+      "LEFT_BOTTOM",
+      "RIGHT_BOTTOM",
+      "BOTTOM",
+    ],
+    ["TOP", "RIGHT_TOP", "LEFT_TOP", "CENTER", "RIGHT_BOTTOM", "BOTTOM"],
+  ];
+
+  const sidesMappedToSegments = new Map();
+  const mappedToDigit = new Map();
+
+  const [input, output] = sequence.split("|");
+
+  digitLengths.forEach((_, index) => {
+    mappedToDigit.set(index, "");
+  });
+
+  sides.forEach((side) => {
+    sidesMappedToSegments.set(side, "");
+  });
+
+  let notKnown = "";
+
+  input
+    .trim()
+    .split(" ")
+    .map((digit) => {
+      if (digit.length === 5 || digit.length === 6) {
+        notKnown += digit;
+      } else {
+        knownDigits.map((known) => {
+          if (digit.length === digitLengths[known]) {
+            mappedToDigit.set(known, digit);
+          }
+        });
+      }
+    });
+
+  const segmentsOccurance = [...new Array(segments.length).fill(0)];
+  segments.forEach((segment, index) => {
+    segmentsOccurance[index] += [...notKnown]
+      .map((letter) => letter === segment)
+      .reduce(addPredicate);
+  });
+
+  sidesMappedToSegments.set(
+    "TOP",
+    [...mappedToDigit.get(7)]
+      .filter((letter) => ![...mappedToDigit.get(1)].includes(letter))
+      .join("")
   );
 
-  const furthestCrab = Math.max(...crabPositions);
-  const moves = [...new Array(furthestCrab).fill(0)];
+  sidesMappedToSegments.set(
+    "LEFT_BOTTOM",
+    segments[segmentsOccurance.findIndex((segment) => segment === 3)]
+  );
 
-  [...new Array(furthestCrab)].forEach((_, position) => {
-    crabPositions.forEach((crab) => {
-      moves[position] += [
-        ...new Array(Math.abs(crab - (position + 1)) + 1).keys(),
-      ].reduce(addPredicate);
+  sidesMappedToSegments.set(
+    "BOTTOM",
+    segments[
+      segmentsOccurance.findIndex(
+        (segment, index) =>
+          segment === 6 && segments[index] != sidesMappedToSegments.get("TOP")
+      )
+    ]
+  );
+
+  sidesMappedToSegments.set(
+    "RIGHT_TOP",
+    segments[
+      segmentsOccurance.findIndex(
+        (segment, index) =>
+          segment === 4 && [...mappedToDigit.get(1)].includes(segments[index])
+      )
+    ]
+  );
+
+  sidesMappedToSegments.set(
+    "RIGHT_BOTTOM",
+    segments[
+      segmentsOccurance.findIndex(
+        (segment, index) =>
+          segment === 5 && [...mappedToDigit.get(1)].includes(segments[index])
+      )
+    ]
+  );
+
+  sidesMappedToSegments.set(
+    "LEFT_TOP",
+    segments[
+      segmentsOccurance.findIndex(
+        (segment, index) =>
+          segment === 4 &&
+          segments[index] != sidesMappedToSegments.get("RIGHT_TOP")
+      )
+    ]
+  );
+
+  sidesMappedToSegments.set(
+    "CENTER",
+    segments[
+      segmentsOccurance.findIndex(
+        (segment, index) =>
+          segment === 5 &&
+          segments[index] != sidesMappedToSegments.get("RIGHT_BOTTOM")
+      )
+    ]
+  );
+
+  let out = "";
+
+  output
+    .trim()
+    .split(" ")
+    .map((digit) => {
+      const toFind = [...digit].sort();
+      digitSides.forEach((structure, index) => {
+        let decrypted = structure
+          .map((side) => sidesMappedToSegments.get(side))
+          .sort();
+        console.log(toFind.join(""), decrypted.join(""));
+        if (toFind.join("") == decrypted.join("")) {
+          out += index.toString();
+        }
+      });
     });
-  });
-  console.log(Math.min(...moves));
-})();
+
+  return out;
+};
